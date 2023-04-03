d9c060dabc222567cc08b625f94d93f701634192
Aryan <aryan.joy@outlook.com>
2023-04-03 14:59:36 -0600

    first commit

---
+++ permitScraper.py
@@ -0,0 +1,30 @@
+"""
+
+Step 1:
+- research open data portal vancouver
+- locate desired dataset 
+- get api 
+
+Step 2: 
+- search fields
+- query data from for loop
+
+
+"""
+import requests
+import json
+api_url = "https://opendata.vancouver.ca/api/v2/catalog/datasets/issued-building-permits/records?select=%2A&limit=10&offset=0&timezone=UTC"
+response = requests.get(api_url)
+data = json.loads(response.text)
+
+
+
+
+"""
+permitnumber, description, status, applicant name
+"""
+
+for i in data["records"]:
+    # print(i["record"]["fields"])
+    print("--------------------------------------------------------------\n")
+    print("PERMIT NUMBER : {pnum}\nDESCRIPTION : {desc}\nAPPLICANT NAME : {a_name}\n".format(pnum = i["record"]["fields"]["permitnumber"], desc = i["record"]["fields"]["projectdescription"], a_name = i["record"]["fields"]["applicant"]))
